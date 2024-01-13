72997b9fdc981007a6992c07f88da3b3ed670009
ChayFadida <chayfadida1997@gmail.com>
2024-01-13 22:13:01 +0200

    change

--- infrastructure/dsl/pipeline_job/common/common.groovy
+++ infrastructure/dsl/pipeline_job/common/common.groovy
@@ -1,18 +1,19 @@
-class CommonSteps {
-    static void applyCpsScm(job, String scmUrl, String credentialsId, String branchName, String scriptFilePath) {
-        job.definition {
-            cpsScm {                
-                scm {
-                    git {
-                        branch branchName
-                        remote {
-                            url scmUrl
-                            credentials credentialsId
-                        }
+def applyCpsScm(job, String scmUrl, String credentialsId, String branchName, String scriptFilePath) {
+    job.definition {
+        cpsScm {                
+            scm {
+                git {
+                    branch branchName
+                    remote {
+                        url scmUrl
+                        credentials credentialsId
                     }
                 }
-                scriptPath(scriptFilePath)
             }
+            scriptPath(scriptFilePath)
         }
     }
 }
+
+
+return this
