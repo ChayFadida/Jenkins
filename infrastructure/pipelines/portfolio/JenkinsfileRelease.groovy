298ef91f98f0421d9cb5069b7253b6f42927a8b9
ChayFadida <chayfadida1997@gmail.com>
2024-01-14 20:45:23 +0200

    fix

--- infrastructure/pipelines/portfolio/JenkinsfileRelease.groovy
+++ infrastructure/pipelines/portfolio/JenkinsfileRelease.groovy
@@ -1,6 +1,9 @@
 pipeline {
-    agent any
-    
+    agent {
+        kubernetes {
+            label 'jenkins-jenkins-agent'
+        }
+    }
     stages {
         stage('Checkout') {
             steps {
