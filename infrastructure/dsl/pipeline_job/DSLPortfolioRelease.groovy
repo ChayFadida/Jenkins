b2079ac6a2edeb420960cc4c226c08a63223a36b
ChayFadida <chayfadida1997@gmail.com>
2024-01-14 10:05:49 +0200

    fix

--- infrastructure/dsl/pipeline_job/DSLPortfolioRelease.groovy
+++ infrastructure/dsl/pipeline_job/DSLPortfolioRelease.groovy
@@ -13,6 +13,14 @@ pipelineJob(JOB_NAME) {
                     }
                 }
             }
+            scm {
+                git {
+                    branch 'master' // Replace with your branch name
+                    remote {
+                        url 'https://github.com/ChayFadida/Jenkins.git' // Replace with your Git repository URL
+                    }
+                }
+            }
             scriptPath "infrastructure/pipelines/alignIp/JenkinsfileAlignIp.groovy"
         }
     }
