32bb6c457755fc3bbab4790df4bf9197e6767687
ChayFadida <chayfadida1997@gmail.com>
2024-01-14 09:18:50 +0200

    fix

--- infrastructure/dsl/pipeline_job/DSLPortfolioRelease.groovy
+++ infrastructure/dsl/pipeline_job/DSLPortfolioRelease.groovy
@@ -4,16 +4,17 @@ def JOB_NAME = "pipelines/alignIp"
 pipelineJob(JOB_NAME) {
     description 'Portfolio Release Pipeline'
 
-    cpsScm {
-        scm {
-            git {
-                branch 'master' // Replace with your branch name
-                remote {
-                    url 'https://github.com/ChayFadida/Utils.git' // Replace with your Git repository URL
+    job.definition {
+        cpsScm {                
+            scm {
+                git {
+                    branch 'master' // Replace with your branch name
+                    remote {
+                        url 'https://github.com/ChayFadida/Utils.git' // Replace with your Git repository URL
+                    }
                 }
             }
         }
     }
-    
     
 }
