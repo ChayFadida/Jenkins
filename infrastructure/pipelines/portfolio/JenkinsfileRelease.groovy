39cc96616dcf5e5c91e8c9ff3745ee76e1b60320
ChayFadida <chayfadida1997@gmail.com>
2024-01-27 16:05:02 +0200

    FIX

--- infrastructure/pipelines/portfolio/JenkinsfileRelease.groovy
+++ infrastructure/pipelines/portfolio/JenkinsfileRelease.groovy
@@ -23,7 +23,7 @@ pipeline {
                     withCredentials([string(credentialsId: 'PORTFOLIO_REACT_APP_EMAILJS_SERVICE_ID', variable: 'REACT_APP_EMAILJS_SERVICE_ID'),
                                      string(credentialsId: 'PORTFOLIO_REACT_APP_EMAILJS_TEMPLATE_ID', variable: 'REACT_APP_EMAILJS_TEMPLATE_ID'),
                                      string(credentialsId: 'PORTFOLIO_REACT_APP_EMAILJS_USER_ID', variable: 'REACT_APP_EMAILJS_USER_ID')]) {
-                        docker.build(path=".", tag="harbor.chay-techs.com/portfolio/portfolio-front:testim", dockerfile="Dockerfile.portfolio")
+                        docker.build(tag="harbor.chay-techs.com/portfolio/portfolio-front:testim", dockerfile="Dockerfile.portfolio")
                     }
                 }
             }
