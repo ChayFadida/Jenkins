26d745eedac3fcc29d92f2a1af6cd1b4d1e3ee83
ChayFadida <chayfadida1997@gmail.com>
2024-01-27 16:40:54 +0200

    fix

--- infrastructure/dsl/pipeline_job/DSLPortfolioRelease.groovy
+++ infrastructure/dsl/pipeline_job/DSLPortfolioRelease.groovy
@@ -4,10 +4,12 @@ pipelineJob(JOB_NAME) {
     description 'Portfolio Release Pipeline'
 
     parameters {
-        stringParam('DOCKER_REGISTRY', 'harbor.chay-techs.com', 'Docker registry URL')
-        stringParam('APP_NAME', 'portfolio-front', 'Name of your application')
-        stringParam('NAMESPACE', 'chay-techs-production', 'Kubernetes namespace')
+        stringParam('DOCKER_REGISTRY', 'https://harbor.chay-techs.com', 'Docker registry URL')
         stringParam('IMAGE_TAG', '', 'Docker image tag')
+    }
+
+    enviromentVariables {
+        env('DOCKER_REGISTRY', 'harbor.chay-techs.com')
     }
     
     definition {
