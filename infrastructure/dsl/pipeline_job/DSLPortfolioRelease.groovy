1c68e169cb2774f0d6e5e555c834dedaa2011a2d
ChayFadida <chayfadida1997@gmail.com>
2024-01-28 21:08:25 +0200

    fix

--- infrastructure/dsl/pipeline_job/DSLPortfolioRelease.groovy
+++ infrastructure/dsl/pipeline_job/DSLPortfolioRelease.groovy
@@ -11,7 +11,7 @@ pipelineJob(JOB_NAME) {
                 }
             }
             regexpFilterText("\$portfolio_branch")
-            regexpFilterExpression("^(refs\/heads\/chay\/kubernetes)*?$")
+            regexpFilterExpression("^(refs\\/heads\\/chay\\/kubernetes)*?\$")
             printContributedVariables(true)
             printPostContent(true)
             token('PORTFOLIO-CI')
