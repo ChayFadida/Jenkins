42b1b3fa1295ff0a9244b2efa826ec5345660483
ChayFadida <chayfadida1997@gmail.com>
2024-01-28 20:47:42 +0200

    fix

--- infrastructure/dsl/pipeline_job/DSLPortfolioRelease.groovy
+++ infrastructure/dsl/pipeline_job/DSLPortfolioRelease.groovy
@@ -8,7 +8,7 @@ pipelineJob(JOB_NAME) {
                 genericVariable {
                     key("portfolio_branch")
                     value("\$.ref")
-                    regexpFilter("refs\/heads\/(.+)")
+                    regexpFilter("refs\\/heads\\/(.+)")
                 }
             }
             regexpFilterText("\$.ref == 'refs/heads/chay/kubernetes'")
