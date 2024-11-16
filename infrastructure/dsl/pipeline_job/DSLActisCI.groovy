6736364c73b96c000c64470aaccb0bc3595b24c1
ChayFadida <chayfadida1997@gmail.com>
2024-11-16 11:16:06 +0200

    add actis ci

---
+++ infrastructure/dsl/pipeline_job/DSLActisCI.groovy
@@ -0,0 +1,40 @@
+def JOB_NAME = "pipelines/Actis-CI"
+
+pipelineJob(JOB_NAME) {
+    description 'Actis CI Pipeline'
+    triggers {
+        genericTrigger {
+            genericVariables {
+                genericVariable {
+                    key("actis_branch")
+                    value("\$.ref")
+                }
+            }
+            regexpFilterText("\$actis_branch")
+            regexpFilterExpression("^(refs\\/heads\\/(master|develop))*?\$")
+            printContributedVariables(true)
+            printPostContent(true)
+            tokenCredentialId('Actis-CI-Webhook-Token')
+        }
+    }
+
+    environmentVariables {
+        env('DOCKER_REGISTRY', 'harbor.chay-techs.com')
+        env('GIT_USERNAME', 'ChayFadida')
+        env('GIT_MAIL', 'chayfadida1997@gmail.com')
+    }
+    
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    branch branchName
+                    remote {
+                        url 'https://github.com/ChayFadida/Jenkins.git'
+                    }
+                }
+            }
+            scriptPath "infrastructure/pipelines/studforstud/JenkinsfileCI.groovy"
+        }
+    }
+}
---
+++ infrastructure/pipelines/actis/JenkinsActis.groovy
@@ -0,0 +1,94 @@
+pipeline {
+    agent {
+        kubernetes {
+            label 'K8S-With-Docker'
+        }
+    }
+    environment {
+        CHECKED_OUT_BRANCH = ''
+        IMAGE_TAG = ''
+    }
+    stages {
+        stage('Checkout Source Code') {
+            steps {
+                cleanWs()
+                dir('actis-src') {
+                    script {
+                        def gitInfo = checkout([$class: 'GitSCM',
+                                branches: [[name: portfolio_branch]],
+                                extensions: [],
+                                submoduleCfg: [],
+                                userRemoteConfigs: [[url: 'https://github.com/ChayFadida/actis.git']]])
+                        CHECKED_OUT_BRANCH = gitInfo.GIT_BRANCH.split('/')[-1]
+                    }
+                }
+
+                dir('actis-cd') {
+                    script {
+                        checkout([$class: 'GitSCM',
+                                branches: [[name: portfolio_branch]],
+                                extensions: [],
+                                submoduleCfg: [],
+                                userRemoteConfigs: [[url: 'https://github.com/ChayFadida/ActisGitOps.git']]])
+                        sh "git checkout ${CHECKED_OUT_BRANCH}"
+                    }
+                }
+            }
+        }
+
+        stage('Build and Push Docker Image') {
+            steps {
+                dir('actis-src') {
+                    sh "ls"
+                    script {
+                        def commitHash = sh(script: 'git rev-parse --short HEAD', returnStdout: true).trim()
+                        IMAGE_TAG = "${CHECKED_OUT_BRANCH}_${commitHash}"
+                        
+                        // Build docker arguments
+                        docker.withRegistry("https://${DOCKER_REGISTRY}", 'harbor-cred-secret') {
+                            def docker_image = docker.build("${DOCKER_REGISTRY}/portfolio/portfolio-front:${IMAGE_TAG}", "-f Dockerfile.portfolio .")
+                            docker_image.push()
+                            sh "docker rmi ${docker_image.id}"
+                        }
+                        
+                    }
+                }
+            }
+        }
+
+        
+        stage('Update Deployment') {
+            steps {
+                dir('portfolio-cd') {
+                    script {            
+                        // Define the path to the Deployment YAML file based on the branch/environment
+                        def deploymentPath = "deployment.yml"
+
+                        // Read the Deployment YAML file
+                        def deploymentYaml = readFile(deploymentPath)
+
+                        // Update the image tag in the Deployment YAML
+                        deploymentYaml = deploymentYaml.replaceAll("(?<=image: harbor\\.chay-techs\\.com\\/actis\\/website:)\\S+", IMAGE_TAG)
+
+                        // Write the modified Deployment YAML back to the file
+                        writeFile(file: deploymentPath, text: deploymentYaml)
+
+                        withCredentials([usernamePassword(credentialsId: 'github-secret-login', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
+                            sh "git config --global user.email ${GIT_MAIL}"
+                            sh "git config --global user.name ${GIT_USERNAME}"
+                            sh "git add ${deploymentPath}"
+                            sh 'git commit -m "Update Docker image tag in deployment.yml"'
+                            sh "git push https://$USERNAME:$PASSWORD@github.com/ChayFadida/PortfolioCD.git"
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    post {
+        success {
+            echo 'Pipeline completed successfully. Pushed changes to the ArgoCD Git repository.'
+        }
+    }
+}
